b618f97459248b8330ba7d7685e5c1555244223a
곽문구 <moongstory@gmail.com>
2024-05-05 15:26:36 +0900

    friend 쿼리문 데이터도 같이 추가되도록 수정

--- Moong/PHP/PHP 프로그래밍 입문/쿼리/friend.sql
+++ Moong/PHP/PHP 프로그래밍 입문/쿼리/friend.sql
@@ -5,3 +5,6 @@ create table friend (
 	address char(80),
 	primary key(num)
 );
+
+insert into member (name, tel, address) values ('송예진', '031-123-4567', '경기도 용인시 수지고 정평로 30');
+insert into member (name, tel, address) values ('김수정', '031-333-4444', '양평군 지평면 묵방길 123번지');
