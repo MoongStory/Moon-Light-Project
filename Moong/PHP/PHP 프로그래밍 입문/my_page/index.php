280c17181a03f393c72c50c81ef58d7df3f9033a
곽문구 <moongstory@gmail.com>
2024-05-06 23:15:29 +0900

    책 따라해보기 프로젝트 시작

---
+++ Moong/PHP/PHP 프로그래밍 입문/my_page/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<header>
+		<?php include "header.php";?>
+	</header>
+
+	<section>
+		<?php include "main.php";?>
+	</section>
+
+	<footer>
+		<?php include "footer.php";?>
+	</footer>
+</body>
+</html>
