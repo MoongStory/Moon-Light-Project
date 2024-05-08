4c29bb1a911d3d5a266c2efd29ae1ea8df03876b
곽문구 <moongstory@gmail.com>
2024-05-08 21:26:42 +0900

    footer 파일 수정

--- Moong/PHP/PHP 프로그래밍 입문/my_page/footer.php
+++ Moong/PHP/PHP 프로그래밍 입문/my_page/footer.php
@@ -1,3 +1,9 @@
-<?php
-	echo "footer";
-?>
+===FOOTER START=================================================
+<div id="footer_content">
+	<p id="footer_logo">FOOTER LOGO TEST | 테스트</p>
+
+	<ul id="author">
+		<li>메일 주소 : moongstory@gmail.com</li>
+	</ul>
+</div>
+===FOOTER END=================================================
