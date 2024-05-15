a4a10a111fc0dc3967fc6c472e846d4266934529
곽문구 <moongstory@gmail.com>
2024-05-15 19:59:45 +0900

    아이디나 비밀번호 없어도 submit 되던 문제 수정

--- Moong/PHP/PHP 프로그래밍 입문/my_page/view_login.php
+++ Moong/PHP/PHP 프로그래밍 입문/my_page/view_login.php
@@ -12,12 +12,16 @@
 			{
 				alert("아이디를 입력해주세요.");
 				document.form_login.id.focus();
+
+				return;
 			}
 
 			if(document.form_login.password.value === "")
 			{
 				alert("비밀번호를 입력해주세요.");
 				document.form_login.password.focus();
+
+				return;
 			}
 
 			document.form_login.submit();
