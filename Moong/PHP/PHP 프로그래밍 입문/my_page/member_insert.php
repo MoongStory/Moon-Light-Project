53cea50275d17d8a97ff78c5473ad60ca1a3ecc2
곽문구 <moongstory@gmail.com>
2024-05-15 17:25:56 +0900

    회원 가입 후, 가입 정보로 바로 로그인 되도록 수정

--- Moong/PHP/PHP 프로그래밍 입문/my_page/member_insert.php
+++ Moong/PHP/PHP 프로그래밍 입문/my_page/member_insert.php
@@ -4,20 +4,30 @@
 	// $password_confirm = $_POST["pass_confirm"];
 	$name = $_POST["name"];
 
-	// echo "아이디 : ".$id."<br>";
-	// echo "비밀번호 : ".$password."<br>";
-	// echo "비밀번호 확인 : ".$password_confirm."<br>";
-	// echo "이름 : ".$name."<br>";
-
-	$regist_day = date("Y-m-d (H:i)");  // 현재의 '년-월-일-시-분'을 저장
+	$regist_day = date("Y-m-d (H:i)");  // 현재의 년-월-일-시-분'을 저장
 
 	$con = mysqli_connect("localhost", "user1", "12345", "my_db");
 
 	$sql = "insert into member(id, password, name, registration_date) ";
 	$sql .= "values('$id', '$password', '$name', '$regist_day')";
 
-	mysqli_query($con, $sql);  // $sql 에 저장된 명령 실행
-    mysqli_close($con);     
+	mysqli_query($con, $sql); // $sql 에 저장된 명령 실행
+
+	// region start 회원 가입 후 가입 정보로 바로 로그인 되도록
+	$sql = "select * from member where id = '$id' and name = '$name'";
+
+	$result = mysqli_query($con, $sql); // $sql 에 저장된 명령 실행
+
+	if (mysqli_num_rows($result))
+	{
+		session_start();
+
+		$_SESSION["user_id"] = $id;
+		$_SESSION["user_name"] = $name;
+	}
+	// region end 회원 가입 후 가입 정보로 바로 로그인 되도록
+
+    mysqli_close($con);
 
     echo "
 		<script>
