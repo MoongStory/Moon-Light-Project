d1901581c9ae2f610b8b0ad7e6f6f033a8b4bf4d
곽문구 <moongstory@gmail.com>
2024-06-03 22:58:15 +0900

    노마드코더, 바닐라 JS로 크롬 앱 만들기, #6.0

---
+++ Moong/자바스크립트/momentum/JS/clock.js
@@ -0,0 +1,14 @@
+const clock = document.querySelector("h2#clock");
+
+function getCurrentTime() {
+	const currentTime = new Date();
+
+	currentTime.get
+
+	return `${currentTime.getHours()}:${currentTime.getMinutes().toString().padStart(2, "0")}:${currentTime.getSeconds().toString().padStart(2, "0")}.${currentTime.getMilliseconds().toString().padStart(3, "0")}`;
+}
+
+clock.innerText = getCurrentTime();
+setInterval(() => {
+	clock.innerText = getCurrentTime();
+}, 1);
