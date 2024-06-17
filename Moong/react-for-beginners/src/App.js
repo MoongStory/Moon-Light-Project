62883cade27b5c007ffc434db56e3e42c30740d6
곽문구 <moongstory@gmail.com>
2024-06-18 00:11:52 +0900

    노마드코더, ReactJS로 영화 웹 서비스 만들기, #7.0

--- Moong/react-for-beginners/src/App.js
+++ Moong/react-for-beginners/src/App.js
@@ -1,31 +1,37 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  useEffect(() => {
-    console.log("hi :)");
-    return () => {
-      console.log("bye :(");
-    };
-  }, []);
-
-//   useEffect(function () {
-//     console.log("hi :)");
-//     return function () {
-//       console.log("bye :(");
-//     };
-//   }, []);
-
-  return <h1>Hello</h1>;
-}
+// import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setTodo] = useState("");
+  const [toDos, setToDos] = useState([]);
+
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    // console.log(toDo);
+	if(toDo === "") {
+		return;
+	}
+
+	setToDos(currentArray => [toDo, ...currentArray]);
+	setTodo("");
+};
+
+console.log(toDos);
 
   return (
     <div>
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-      {showing ? <Hello /> : null}
+	<h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        ></input>
+        <br />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
