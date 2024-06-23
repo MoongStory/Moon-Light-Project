33fae2a1af629244de574f73d83d36cb85edad42
곽문구 <moongstory@gmail.com>
2024-06-23 23:11:22 +0900

    노마드코더, ReactJS로 영화 웹 서비스 만들기, #7.3

--- Moong/react-for-beginners/src/App.js
+++ Moong/react-for-beginners/src/App.js
@@ -1,48 +1,7 @@
-import { useState, useEffect } from "react";
+import Home from './routes/Home.js'
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-      )
-    ).json();
-
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  console.log(movies);
-
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} alt="movie_cover_image" />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-              <hr />
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
