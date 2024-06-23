eda4972f0588fd2c76f42f6793973656ac727d88
곽문구 <moongstory@gmail.com>
2024-06-23 16:56:05 +0900

    노마드코더, ReactJS로 영화 웹 서비스 만들기, #7.3

--- Moong/react-for-beginners/src/App.js
+++ Moong/react-for-beginners/src/App.js
@@ -1,35 +1,45 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [Loading, setLoading] = useState(true);
-  const [Coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json();
+
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
 
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers").then((response) =>
-      response.json().then((json) => {
-        setCoins(json);
-        setLoading(false);
-      })
-    );
-
-    // return () => {
-    //   second
-    // }
+    getMovies();
   }, []);
+
+  console.log(movies);
 
   return (
     <div>
-      <h1>The Coins! {Loading ? "" : `(${Coins.length})`}</h1>
-      {Loading ? (
-        <strong>Loadings...</strong>
+      {loading ? (
+        <h1>Loading...</h1>
       ) : (
-        <select>
-          {Coins.map((coin) => (
-            <option>
-              {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
-            </option>
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+			<img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+              <hr />
+            </div>
           ))}
-        </select>
+        </div>
       )}
     </div>
   );
